3f5d6c68c5d4c3f05bcd53b8bdf13229314861a9
bonner72 <john@bahler.us>
2025-07-30 13:24:59 -0500

    Changed terminal text size
    Changed terminal text size from 18 to 15.

--- SerialTerminal/windows-amd64/source/SerialTerminal.java
+++ SerialTerminal/windows-amd64/source/SerialTerminal.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27695f0bca367b3862466fbea90b5b6f6b836f113fd9e36a9f8290017e27f743
-size 12750
+oid sha256:0c3c27db91756f9a033bcb7c41946ab46213d4f1b199df571e522417ad17e3e8
+size 12751
--- SerialTerminal/windows-amd64/source/SerialTerminal.pde
+++ SerialTerminal/windows-amd64/source/SerialTerminal.pde
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9856a78a2dda84fe5a079fe6e7a14b7392cc0c95bc601505b41f09dee43d3f15
+oid sha256:debc9443300fc72c31202bb024bf928d57d3dcf26bc84d0733b6707aea35b579
 size 1658
