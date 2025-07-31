6ad33680b59ec58539b0fcf37e9854afd7efe102
bonner72 <john@bahler.us>
2025-07-31 18:23:22 -0500

    Bug Fixes
    Selected port would not show in connect button on change.

--- SerialTerminal/windows-amd64/source/SerialTerminal.java
+++ SerialTerminal/windows-amd64/source/SerialTerminal.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b100adfa72b5c6e6eb167c75c9d24a253e2eedaa35b8c7caa729b17dd74dc15
-size 16878
+oid sha256:b6624dba3b3ce601079a6706567aab71858d32b042dd517d69b82bbb72c087f6
+size 17068
